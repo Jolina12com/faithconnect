d30a4d3974163dfaa1a136c062e9671f97502ca0
Jolina Soriano <jolinasoriano996@gmail.com>
2025-10-30 11:17:29 +0800

    Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Pagination\Paginator;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,14 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         // Register custom middleware
         $this->app['router']->aliasMiddleware('cors', \App\Http\Middleware\HandleCors::class);
-        
+
         // Register observers
         \Illuminate\Notifications\DatabaseNotification::observe(\App\Observers\NotificationObserver::class);
-        
+
         Paginator::useBootstrap();
     }
 }
